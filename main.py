7451b06322911893f023a7e9b03bb0455988e9c7
Niranjan B <niranjannb7777@gmail.com>
2025-10-09 10:11:19 +0530

    chore: remove unused langgraph react agent import

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 from dotenv import load_dotenv
 
 from langchain_openai import ChatOpenAI
-from langgraph.prebuilt import create_react_agent
 from langchain.tools import tool
 
 load_dotenv()
